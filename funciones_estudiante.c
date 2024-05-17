b92f9bb552b2760c6ec023ddffcf8589067a3ec6
Gustavo <gustavotobiasp@gmail.com>
2024-05-16 21:12:48 -0300

    Comiezo

---
+++ funciones_estudiante.c
@@ -0,0 +1,33 @@
+/*
+    Integrantes del grupo. En caso de ser un grupo de dos integrantes, no completar el último campo.
+    Si alguno de los integrantes del grupo dejara la materia, completar de todos modos sus datos, aclarando que no entrega.
+    -----------------
+    Apellido: Perla
+    Nombre: Gustavo Tobias
+    DNI: 43386388
+    Entrega: Si
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+*/
+
+#include "funciones_estudiante.h"
+
+void* solucion(int* argc, char* argv[])
+{
+    if(*argc<=1){
+        puts("No hay Argumentos suficientes");
+        return SIN_PARAMETROS;
+    }
+
+
+
+}
